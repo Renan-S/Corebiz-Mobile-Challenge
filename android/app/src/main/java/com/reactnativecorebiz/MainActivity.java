b88ec465b24f090cfdc889bbd747c3371ef3732c
Renan Cavalcante <renan.cav.ti@gmail.com>
2021-08-29 16:24:11 -0300

    Change project name

--- android/app/src/main/java/com/reactnativecorebiz/MainActivity.java
+++ android/app/src/main/java/com/reactnativecorebiz/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativecorebiz;
+package com.reactnativestarwars;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "ReactNativeCorebiz";
+    return "ReactNativeStarWars";
   }
 }
--- android/app/src/main/java/com/reactnativecorebiz/MainApplication.java
+++ android/app/src/main/java/com/reactnativecorebiz/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativecorebiz;
+package com.reactnativestarwars;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.reactnativecorebiz.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.reactnativestarwars.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
